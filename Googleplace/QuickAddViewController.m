b0a1646560a85f248afa857009efe30d6336451f
roei <roei46@yahoo.com>
2016-10-20 19:40:12 +0300

    magnifying glass color

--- Googleplace/QuickAddViewController.m
+++ Googleplace/QuickAddViewController.m
@@ -94,13 +94,15 @@
     
     
     
+    _Feedback2.placeholder = @"Please add your feedback here";
 
-
-  _Feedback2.placeholder = @"Please add your feedback here";
-    
     
     UITextField *searchField = [_searchController.searchBar valueForKey:@"_searchField"];
     
+    UIImageView *imageV = searchField.leftView;
+    imageV.image = [imageV.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    imageV.tintColor = [UIColor colorWithRed:0.09 green:0.36 blue:0.41 alpha:1.0];
+
     
     // Change the search bar placeholder text color
     [searchField setValue:[UIColor colorWithRed:0.09 green:0.36 blue:0.41 alpha:1.0]
--- Googleplace/ViewController.m
+++ Googleplace/ViewController.m
@@ -97,7 +97,10 @@ static NSString const *kTerrainType = @"Terrain";
     
     
     UITextField *searchField = [_searchController.searchBar valueForKey:@"_searchField"];
-    
+    UIImageView *imageV = searchField.leftView;
+    imageV.image = [imageV.image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    imageV.tintColor = [UIColor colorWithRed:0.09 green:0.36 blue:0.41 alpha:1.0];
+
     
     // Change the search bar placeholder text color
     [searchField setValue:[UIColor colorWithRed:0.09 green:0.36 blue:0.41 alpha:1.0]
